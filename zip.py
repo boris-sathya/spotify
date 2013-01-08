03d82fb175ef0f2da9504e404c99b59fa74586a4
boris-sathya <sathyaprakash222@gmail.com>
2013-01-08 17:33:29 +0100

    Debugged Duplicate key issue in dict

--- zip.py
+++ zip.py
@@ -1,4 +1,5 @@
 import sys
+from operator import itemgetter
 inp = sys.stdin.readlines()
 splits = []
 rank = []
@@ -9,22 +10,27 @@ while len(inp) > i:
     i = i + 1
 #calculate rank using zipf's law
 #store ranks in a list
-#for i in range(9):
-#    print splits[i]
 
 i = 1
 while i <= int(splits[0]):
     testval = int(splits[i*2])
     ref = int(splits[2])/i
     q = float(testval)/float(ref)
-    rank.append(q)
-    songs[q] = splits[(i*2)+1]
+    meta = []
+    meta.append(splits[(i*2)+1])
+    meta.append(i)
+    ranked_meta = []
+    ranked_meta.append(q)
+    ranked_meta.append(meta)
+    rank.append(ranked_meta)
     i = i+1
 
 #sort the list according to quality rank(qi) and fetch the corresponding song's#name from dictionary
 i=1
-rank.sort(key=int)
+rank.sort(key=itemgetter(0))
 while int(splits[1]) >= i:
-    print songs.get(rank[-i])
+    if(rank[-i][0] == rank[-i-1][0]):
+        rank[-i], rank[-i-1] = rank[-i-1],rank[-i]     
+    print rank[-i][1][0]
     i = i + 1
 
