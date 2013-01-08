05a5e47a1f474725a32a5450ba6f8f84786f52a9
boris-sathya <sathyaprakash222@gmail.com>
2013-01-08 03:30:33 +0100

    comments and input test cases added

--- zip.py
+++ zip.py
@@ -1,22 +1,22 @@
 inp = raw_input()
 splits = inp.split()
-q = 0.0
 rank = []
 songs = {}
-i=1
-
-while i<=int(splits[0]):
-    i = i + 1
+#calculate rank using zipf's law
+#store ranks in a list
 i = 1
-while i<=int(splits[0]):
+while i <= int(splits[0]):
     testval = int(splits[i*2])
     ref = int(splits[2])/i
     q = float(testval)/float(ref)
     rank.append(q)
     songs[q] = splits[(i*2)+1]
     i = i+1
-rank.sort(key=int)
+
+#sort the list according to quality rank(qi) and fetch the corresponding song's#name from dictionary
 i=1
+rank.sort(key=int)
 while int(splits[1]) >= i:
     print songs.get(rank[-i])
     i = i + 1
+
