812bd1f5ebd5c8376662096fe6e5ee9d5af32b54
boris-sathya <sathyaprakash222@gmail.com>
2013-11-13 19:27:44 +0100

    wrapped up

--- zip.py
+++ zip.py
@@ -1,32 +1,24 @@
 import sys
-from operator import itemgetter
-inp = sys.stdin.readlines()
+inp = sys.stdin.readline()
 splits = []
 songlist = []
 i = 0
 
-#Chops Input Data into a list
-while len(inp) > i:
-    splits = splits + (inp[i].split())
-    i = i + 1
+#total records to extract
+results = inp.split()[1]
 
 #calculate rank using zipf's law
-#store rank and  meta data in a list of dictionaries
+#store rank and meta data in a list of dictionaries
 i = 1
-while i <= int(splits[0]):
-    testval = int(splits[i*2])
-    ref = int(splits[2])/i
-    q = float(testval)/float(ref)
-    meta = {}
-    meta['id'] = i
-    meta['name'] = splits[(i*2)+1]
-    meta['rank'] = q
-    songlist.append(meta)
+for line in sys.stdin:
+    splits = line.split()
+    songlist.append({'name' : splits[1], 'rank' : long(splits[0])*i}) 
     i = i+1
 
-#two key sort (Rev Sort based on rank and Sort based on ID)
-songlist.sort(key=itemgetter('rank'),reverse=True)
+#sort based on rank
+songlist.sort(key=lambda p:p['rank'],reverse=True)
+
 i = 0
-while int(splits[1]) > i:
+while int(results) > i:
     print songlist[i]['name']
     i = i + 1
